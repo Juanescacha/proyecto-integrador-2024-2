4d4687982b01d244fca4237064c3c22b9d5d6efa
Juan Esteban Camargo <juanescacha@hotmail.com>
2024-09-02 21:33:40 -0500

    feat: :sparkles: moving cube

--- src/World.jsx
+++ src/World.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import Cube from "@/Cube"
 
 const World = () => {
-	const cameraSettings = { position: [2, 0, 5] }
+	const cameraSettings = { position: [2, 0, 5], fov: 75 }
 
 	return (
 		<>
 			<Canvas camera={cameraSettings}>
-				<OrbitControls />
-				<ambientLight intensity={1.5} />
-				<directionalLight position={[0, 10, 10]} />
+				<OrbitControls enablePan={false} />
+				<ambientLight intensity={1.7} />
+				<directionalLight position={[0, 10, 0]} />
+				<Cube />
 			</Canvas>
 		</>
 	)
